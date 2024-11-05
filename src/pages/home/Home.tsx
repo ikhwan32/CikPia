233a9767283ff248aae85571ee2e82b87b57f3a4
Ikhwan Hadi <ikhwanhadi32@gmail.com>
2024-11-05 23:22:52 +0800

    test2

--- src/pages/home/Home.tsx
+++ src/pages/home/Home.tsx
@@ -1,5 +1,5 @@
 import "./Home.css";
-import mainImage from "../../assets/anis-sofia.JPG";
+import mainImage from "../../assets/anis-sofia.jpg";
 
 import Marquee from "../../components/Marquee/Marquee";
 import { motion } from "framer-motion";
